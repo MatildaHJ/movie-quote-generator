91ff5072a3f547f7f73f5985b4121b1f3c948110
MatildaHJ <matilda.hj.larsson@gmail.com>
2023-12-07 17:18:58 +0100

    improved UX

--- src/script.js
+++ src/script.js
@@ -14,9 +14,15 @@ function generateQuote(event) {
   let apiKey = "bf3o0930bd56bb8b43fbe8a4cftca0a1";
   let prompt = `User instructions: Generate a quote from the movie ${instructionsInput.value}`;
   let context =
-    "You are a polite AI assistant that has seen every movie ever made and remember them fully. Please provide a short quote. After the quote on a new row with - before, write which charachter said the qoute. Make sure to follow the user instructions.";
+    "You are a polite AI assistant that has seen every movie ever made and remember them fully. Please provide a short quote. After the quote with - before, write which charachter said the qoute and then add one appropriate emoji next to the name. Make sure to follow the user instructions an don't add more than one emoji.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let quoteContainerElement = document.querySelector(".quote-container");
+  quoteContainerElement.classList.remove("hidden");
+
+  let quoteElement = document.querySelector("#quote");
+
+  quoteElement.innerHTML = `<div class="blink">Generating a quote from ${instructionsInput.value}, please wait..🎬</div>`;
   axios.get(apiUrl).then(displayQuote);
 }
 
