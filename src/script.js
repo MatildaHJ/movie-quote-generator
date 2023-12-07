368bbcdc17a55af4cca64af7693ecf2d1def93ef
MatildaHJ <matilda.hj.larsson@gmail.com>
2023-12-07 16:33:29 +0100

    AI context update

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ function generateQuote(event) {
   let apiKey = "bf3o0930bd56bb8b43fbe8a4cftca0a1";
   let prompt = `User instructions: Generate a quote from the movie ${instructionsInput.value}`;
   let context =
-    "You are a polite AI assistant that has seen every movie ever made and remember them fully. Please provide a short quote and on a new row with - before, write which charachter said the qoute. Make sure to follow the user instructions.";
+    "You are a polite AI assistant that has seen every movie ever made and remember them fully. Please provide a short quote. After the quote on a new row with - before, write which charachter said the qoute. Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayQuote);
